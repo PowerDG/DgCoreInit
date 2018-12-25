29c4b59d5225c67d2513a110c060a7b5b5a5de75
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2018-12-25 22:57:36 +0800

    Revert "Initial commit"
    This reverts commit 68bf797ee11ed038c03e6ede0c40b047ae09d14a.

--- Broadview/testRequests/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-url1='http://www.cntour.cn/'
-strhtml=requests.get(url1)
-# strhtml=requests.get('http://www.cntour.cn/')
-# strhtml2=requests.get('http://www.cntour.cn/')
-print(strhtml.text)
