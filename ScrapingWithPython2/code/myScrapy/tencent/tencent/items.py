5b983207deeea3eca39f8564288cfb5c9d42d1d1
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-02-06 08:41:26 +0800

    Update items.py

--- ScrapingWithPython2/code/myScrapy/tencent/tencent/items.py
+++ ScrapingWithPython2/code/myScrapy/tencent/tencent/items.py
@@ -11,4 +11,16 @@ import scrapy
 class TencentItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    position_name = scrapy.Field()
+    position_link = scrapy.Field()
+    position_type = scrapy.Field()
+    people_num = scrapy.Field()
+    work_address = scrapy.Field()
+    publish_time = scrapy.Field()
+
+#
+# ---------------------
+# 作者：a289237642
+# 来源：CSDN
+# 原文：https: // blog.csdn.net / a289237642 / article / details / 80988583
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
